b8e79b6fb2ac6868d6c66a8ec425bc88b71b3911
maksmt <maksmtoff@gmail.com>
2022-01-03 11:37:07 +0200

    first commit

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -2,8 +2,14 @@ $(document).ready(function () {
     $('.team__slider').slick({
         slidesToShow: 4,
         // slidesToScroll: 2,
-        dots: true,
         arrows: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                dots: true,
+                slidesToShow: 2,
+            }
+        }]
     });
 });
 
@@ -14,6 +20,13 @@ $(document).ready(function () {
         dots: true,
         arrows: false,
         speed: 1500,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesPerRow: 2,
+
+            }
+        }]
     });
 });
 
@@ -22,35 +35,42 @@ var scrolled;
 window.onscroll = function () {
     scrolled = window.pageYOffset || document.documentElement.scrollTop;
     if (scrolled > 100) {
-        $(".header").css({ "background": "#87509c" })
+        $(".header").css({
+            "background": "#87509c"
+        })
     }
-    if (scrolled > 40) {
+    if (scrolled > 50) {
         $(".header").css({
             "height": "100px",
-            "padding-top": "-15px",
 
         })
         $(".header-menu").css({
-            "top": "-15px"
-        })
-    }
-    if (scrolled < 40) {
-        $(".header").css({
-            "height": "144px"
-        })
-    }
-    if (scrolled > 400) {
-        $(".header").css({ "background": "#17c2a4" })
+            "padding-top": "25px",
+            "color": "#000",
 
+        })
+
+    }
+
+    if (scrolled < 50) {
+        $(".header").css({
+            "height": "144px",
+
+        })
+    }
+    if (scrolled > 460) {
+        $(".header").css({ "background": "#17c2a4" })
+        $(".navbar-nav").css({
+            "color": "black",
+
+        })
     }
     if (scrolled > 1270) {
-        $(".header").css({ "background": "#e7f1f8" })
+        $(".header").css({
+            "background": "#e7f1f8",
+        })
     }
-    if (scrolled > 2150) {
-        $(".header").css({ "background": "#fff" })
-        $(".header__menu").css({ "color": "#e7f1f8" })
 
-    }
     if (scrolled > 2790) {
         $(".header").css({ "background": "#ffdd99" })
     }
@@ -61,4 +81,9 @@ window.onscroll = function () {
         $(".header").css({ "background": "#3c5499" })
     }
 
+}
+// Создаем медиа условие, проверяющее viewports на ширину не менее 768 пикселей.
+const mediaQuery = window.matchMedia('(max-width: 768px)')
+if (mediaQuery.matches && scrolled > 5630) {
+    $(".header").css({ "background": "#d74680" })
 }
